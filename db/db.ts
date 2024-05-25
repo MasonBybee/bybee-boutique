687a8e7cad295c510d71cf75975e20986d003cd8
Mason Bybee <bybeemason@gmail.com>
2024-05-25 14:18:59 -0500

    moved client to export

--- db/db.ts
+++ db/db.ts
@@ -2,11 +2,9 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 import "dotenv/config";
 
-const client = new Client({
+export const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
 
 client.connect();
-const db = drizzle(client);
-
-export default db;
+export const db = drizzle(client);
