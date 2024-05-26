466cc1c2b71f972e9206c577809a55a887c47cac
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:19:37 -0500

    added user and items relations

--- schema/Wishlist.ts
+++ schema/Wishlist.ts
@@ -1,5 +1,7 @@
 import { pgTable, serial, integer } from "drizzle-orm/pg-core";
 import { users } from "./User";
+import { relations } from "drizzle-orm";
+import { wishlistItems } from "./WishlistItems";
 
 export const wishlists = pgTable("wishlists", {
   id: serial("id").primaryKey(),
@@ -7,6 +9,14 @@ export const wishlists = pgTable("wishlists", {
     .references(() => users.id)
     .notNull(),
 });
+
+export const wishlistRelations = relations(wishlists, ({ one, many }) => ({
+  items: many(wishlistItems),
+  user: one(users, {
+    fields: [wishlists.userId],
+    references: [users.id],
+  }),
+}));
 
 export type Wishlist = typeof wishlists.$inferSelect;
 export type NewWishlist = typeof wishlists.$inferInsert;
