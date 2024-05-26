cf31482b889ee5a39b9e906393db9d2e9a0338fd
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:23:08 -0500

    added category routes

---
+++ app/api/categories/[name]/route.ts
@@ -0,0 +1,33 @@
+import db from "@/db/db";
+import { categories } from "@/schema/Category";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+interface data {
+  name: string;
+  description: string;
+}
+
+export async function GET(request: Request, context: any) {
+  const { params } = context;
+  const category = await db.query.categories.findFirst({
+    where: (categories, { eq }) => eq(categories.name, params.name),
+    with: {
+      products: true,
+    },
+  });
+
+  return NextResponse.json(category);
+}
+
+export async function PATCH(request: Request, context: any) {
+  const { params } = context;
+  const data: data = await request.json();
+  const response = await db
+    .update(categories)
+    .set(data)
+    .where(eq(categories.name, params.name))
+    .returning();
+
+  return NextResponse.json(response);
+}
