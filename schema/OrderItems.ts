81cd5889bbc071d0c5d2e01f658f40d8e34b1a47
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:17:35 -0500

    added order and product relations

--- schema/OrderItems.ts
+++ schema/OrderItems.ts
@@ -1,6 +1,7 @@
-import { pgTable, serial, integer, decimal } from "drizzle-orm/pg-core";
+import { pgTable, serial, integer, text } from "drizzle-orm/pg-core";
 import { orders } from "./Order";
 import { inventory } from "./Inventory";
+import { relations } from "drizzle-orm";
 
 export const orderItems = pgTable("order_items", {
   id: serial("id").primaryKey(),
@@ -10,9 +11,20 @@ export const orderItems = pgTable("order_items", {
   inventoryId: integer("inventory_id")
     .references(() => inventory.id)
     .notNull(),
-  price: decimal("price").notNull(),
+  price: text("price").notNull(),
   quantity: integer("quantity").notNull(),
 });
+
+export const orderItemRelations = relations(orderItems, ({ one }) => ({
+  order: one(orders, {
+    fields: [orderItems.orderId],
+    references: [orders.id],
+  }),
+  product: one(inventory, {
+    fields: [orderItems.inventoryId],
+    references: [inventory.id],
+  }),
+}));
 
 export type OrderItem = typeof orderItems.$inferSelect;
 export type NewOrderItem = typeof orderItems.$inferInsert;
