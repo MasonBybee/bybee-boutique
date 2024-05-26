f95655fcdd1c269d1849322387f3903f1bf4659d
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:16:38 -0500

    added images relations

--- schema/Inventory.ts
+++ schema/Inventory.ts
@@ -1,16 +1,22 @@
-import { pgTable, serial, text, integer, decimal } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
 import { categories } from "./Category";
+import { relations } from "drizzle-orm";
+import { images } from "./Image";
 
 export const inventory = pgTable("inventory", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
   description: text("description").notNull(),
   stock: integer("stock").notNull(),
-  unitPrice: decimal("unit_price").notNull(),
+  unitPrice: text("unit_price").notNull(),
   categoryId: integer("category_id")
     .references(() => categories.id)
     .notNull(),
 });
+
+export const inventoryRelations = relations(inventory, ({ one, many }) => ({
+  images: many(images),
+}));
 
 export type Inventory = typeof inventory.$inferSelect;
 export type NewInventory = typeof inventory.$inferInsert;
