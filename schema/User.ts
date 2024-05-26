7c05f28773eb2d5678a5ebf9a11ae089d4cc9c71
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:19:04 -0500

    added cart, wishlist, reviews, and order relations

--- schema/User.ts
+++ schema/User.ts
@@ -1,4 +1,9 @@
+import { relations } from "drizzle-orm";
 import { pgTable, serial, text, boolean, varchar } from "drizzle-orm/pg-core";
+import { carts } from "./Cart";
+import { wishlists } from "./Wishlist";
+import { reviews } from "./Review";
+import { orders } from "./Order";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -9,6 +14,19 @@ export const users = pgTable("users", {
   email: varchar("email", { length: 150 }).notNull(),
   admin: boolean("admin").default(false).notNull(),
 });
+
+export const userRelations = relations(users, ({ one, many }) => ({
+  cart: one(carts, {
+    fields: [users.id],
+    references: [carts.userId],
+  }),
+  wishlist: one(wishlists, {
+    fields: [users.id],
+    references: [wishlists.userId],
+  }),
+  reviews: many(reviews),
+  orders: many(orders),
+}));
 
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
