70af6aebd48a4f53b54e0683b873ba9aaf9fdd2e
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:14:47 -0500

    added cartitem and user relations

--- schema/Cart.ts
+++ schema/Cart.ts
@@ -1,12 +1,22 @@
-import { pgTable, serial, integer, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, integer, text } from "drizzle-orm/pg-core";
 import { users } from "./User";
+import { relations } from "drizzle-orm";
+import { cartItems } from "./CartItems";
 
 export const carts = pgTable("carts", {
   id: serial("id").primaryKey(),
   userId: integer("user_id").references(() => users.id),
-  createdAt: timestamp("created_at", { withTimezone: true }).notNull(),
-  updatedAt: timestamp("updated_at", { withTimezone: true }).notNull(),
+  createdAt: text("created_at").notNull(),
+  updatedAt: text("updated_at").notNull(),
 });
+
+export const cartRelations = relations(carts, ({ one, many }) => ({
+  items: many(cartItems),
+  user: one(users, {
+    fields: [carts.userId],
+    references: [users.id],
+  }),
+}));
 
 export type Cart = typeof carts.$inferSelect;
 export type NewCart = typeof carts.$inferInsert;
