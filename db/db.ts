7288534f39b4536dbfa9b547e8edd049526f3aeb
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:14:04 -0500

    added relations to db

--- db/db.ts
+++ db/db.ts
@@ -1,10 +1,48 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 import "dotenv/config";
+import { cartRelations, carts } from "@/schema/Cart";
+import { cartItemRelations, cartItems } from "@/schema/CartItems";
+import { categories, categoryRelations } from "@/schema/Category";
+import { imageRelations, images } from "@/schema/Image";
+import { inventory, inventoryRelations } from "@/schema/Inventory";
+import { orderRelations, orders } from "@/schema/Order";
+import { orderItemRelations, orderItems } from "@/schema/OrderItems";
+import { reviewRelations, reviews } from "@/schema/Review";
+import { userRelations, users } from "@/schema/User";
+import { wishlistRelations, wishlists } from "@/schema/Wishlist";
+import { wishlistItemRelations, wishlistItems } from "@/schema/WishlistItems";
 
-export const client = new Client({
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
 
 client.connect();
-export const db = drizzle(client);
+
+const schema = {
+  carts,
+  cartItems,
+  categories,
+  images,
+  inventory,
+  orders,
+  orderItems,
+  reviews,
+  users,
+  wishlists,
+  wishlistItems,
+  cartRelations,
+  cartItemRelations,
+  categoryRelations,
+  imageRelations,
+  inventoryRelations,
+  orderRelations,
+  orderItemRelations,
+  reviewRelations,
+  userRelations,
+  wishlistRelations,
+  wishlistItemRelations,
+};
+
+const db = drizzle(client, { schema });
+export default db;
