cb0935c014f866257127155021d0d00f6f62c00a
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:18:16 -0500

    added user and product relations

--- schema/Review.ts
+++ schema/Review.ts
@@ -1,12 +1,7 @@
-import {
-  integer,
-  pgTable,
-  serial,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 import { users } from "./User";
 import { inventory } from "./Inventory";
+import { relations } from "drizzle-orm";
 
 export const reviews = pgTable("reviews", {
   id: serial("id").primaryKey(),
@@ -16,8 +11,19 @@ export const reviews = pgTable("reviews", {
   inventoryId: integer("inventory_id").references(() => inventory.id),
   rating: integer("rating").notNull(),
   comment: varchar("comment", { length: 240 }).notNull(),
-  reviewDate: timestamp("review_date", { withTimezone: true }).notNull(),
+  reviewDate: text("review_date").notNull(),
 });
+
+export const reviewRelations = relations(reviews, ({ one, many }) => ({
+  user: one(users, {
+    fields: [reviews.userId],
+    references: [users.id],
+  }),
+  product: one(inventory, {
+    fields: [reviews.inventoryId],
+    references: [inventory.id],
+  }),
+}));
 
 export type Review = typeof reviews.$inferSelect;
 export type NewReview = typeof reviews.$inferInsert;
