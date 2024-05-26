d1791d9e8a8e1c9b41a1777c3b20b9564cf2e7b4
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:15:12 -0500

    added cart and product relations

--- schema/CartItems.ts
+++ schema/CartItems.ts
@@ -1,6 +1,7 @@
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial } from "drizzle-orm/pg-core";
 import { carts } from "./Cart";
 import { inventory } from "./Inventory";
+import { relations } from "drizzle-orm";
 
 export const cartItems = pgTable("cart_items", {
   id: serial("id").primaryKey(),
@@ -12,6 +13,17 @@ export const cartItems = pgTable("cart_items", {
     .notNull(),
   quantity: integer("quantity").notNull(),
 });
+
+export const cartItemRelations = relations(cartItems, ({ one }) => ({
+  cart: one(carts, {
+    fields: [cartItems.cartId],
+    references: [carts.id],
+  }),
+  product: one(inventory, {
+    fields: [cartItems.inventoryId],
+    references: [inventory.id],
+  }),
+}));
 
 export type Category = typeof cartItems.$inferSelect;
 export type NewCategory = typeof cartItems.$inferInsert;
