9ba2d955de4ab1b0f41ae2a843fe49c0f5782e29
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:17:00 -0500

    added user and item relations

--- schema/Order.ts
+++ schema/Order.ts
@@ -1,24 +1,27 @@
-import {
-  pgTable,
-  serial,
-  text,
-  integer,
-  decimal,
-  timestamp,
-} from "drizzle-orm/pg-core";
+import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
 import { users } from "./User";
+import { relations } from "drizzle-orm";
+import { orderItems } from "./OrderItems";
 
 export const orders = pgTable("orders", {
   id: serial("id").primaryKey(),
   userId: integer("user_id")
     .references(() => users.id)
     .notNull(),
-  subtotal: decimal("subtotal").notNull(),
-  discount: decimal("discount").notNull(),
-  tax: decimal("tax").notNull(),
+  subtotal: text("subtotal").notNull(),
+  discount: text("discount").notNull(),
+  tax: text("tax").notNull(),
   status: text("status").notNull(),
-  orderDate: timestamp("order_date", { withTimezone: true }),
+  orderDate: text("order_date").notNull(),
 });
+
+export const orderRelations = relations(orders, ({ one, many }) => ({
+  user: one(users, {
+    fields: [orders.userId],
+    references: [users.id],
+  }),
+  items: many(orderItems),
+}));
 
 export type Order = typeof orders.$inferSelect;
 export type NewOrder = typeof orders.$inferInsert;
