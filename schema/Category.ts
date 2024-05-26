6b1368199dad26ab6133a4a6dbba5ee68a5e03c5
Mason Bybee <bybeemason@gmail.com>
2024-05-25 20:15:45 -0500

    added image and product relations

--- schema/Category.ts
+++ schema/Category.ts
@@ -1,10 +1,21 @@
+import { relations } from "drizzle-orm";
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { images } from "./Image";
+import { inventory } from "./Inventory";
 
 export const categories = pgTable("categories", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
   description: text("description").notNull(),
 });
+
+export const categoryRelations = relations(categories, ({ one, many }) => ({
+  image: one(images, {
+    fields: [categories.id],
+    references: [images.categoryId],
+  }),
+  products: many(inventory),
+}));
 
 export type Category = typeof categories.$inferSelect;
 export type NewCategory = typeof categories.$inferInsert;
