de45eb3174955b077f639d55008a9ca394607f60
Mason Bybee <bybeemason@gmail.com>
2024-05-31 17:38:47 -0500

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
-import Image from "next/image";
-import Link from "next/link";
+"use server";
+import CategoryContainer from "@/components/CategoryContainer";
 import styles from "./page.module.css";
-import ProductCard from "../components/ProductCard";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello World</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+      <div className={styles.welcomediv}>
+        <h1 className={styles.welcomeh1}>Welcome to the Bybee Boutique</h1>
+        <h3 className={styles.welcomeh3}>Buzz around and explore our hive!</h3>
+        <p className={styles.disclaimer}>
+          Disclaimer: This website is a proof of concept for my{" "}
+          <a
+            target="_blank"
+            className={styles.link}
+            href={"https://masonbybee.com"}
+          >
+            portfolio
+          </a>
+          , none of the products listed here are real.
+        </p>
+      </div>
+      <CategoryContainer />
     </main>
   );
 }
