f6df24cca563f9bfadce62c67c79886466f2b97e
Mason Bybee <bybeemason@gmail.com>
2024-05-31 17:40:44 -0500

    add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footerContainer">
+      <div className="footerContent">
+        <h3>Helpful Links</h3>
+        <ul></ul>
+      </div>
+      <div className="footerContent">
+        <h3>About Us</h3>
+        <ul></ul>
+      </div>
+      <div className="footerContent">
+        <h3>My Account</h3>
+        <ul></ul>
+      </div>
+      <div className="footerContent">
+        <h3>Contact Us</h3>
+        <ul></ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
