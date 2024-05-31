15232d2ea627b79ffc576f37e6906f5fed0747dd
Mason Bybee <bybeemason@gmail.com>
2024-05-31 17:41:12 -0500

    add loading comp

---
+++ components/Loading.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loadingContainer">
+      <div className="loadingDiv">
+        <img
+          className="loading"
+          src="https://res.cloudinary.com/dabfhr2dr/image/upload/v1716914561/pixilart-drawing_1_bwxpss.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
