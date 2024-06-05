09fe091dab0249524b6b273c027038406dbeba3c
Mason Bybee <bybeemason@gmail.com>
2024-06-04 23:48:05 -0500

    added handleClick for links

--- components/LoginSignUpCard/LoginSignUpCard.tsx
+++ components/LoginSignUpCard/LoginSignUpCard.tsx
@@ -2,19 +2,39 @@ import React from "react";
 import Link from "next/link";
 import styles from "./LoginSignUpCard.module.css";
 
-const LoginSignUpCard = () => {
+interface LoginSignUpCardProps {
+  toggle: () => void;
+}
+
+const LoginSignUpCard: React.FC<LoginSignUpCardProps> = ({ toggle }) => {
+  const handleClick = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    href: string
+  ) => {
+    event.preventDefault();
+    toggle();
+    window.location.href = href;
+  };
+
   return (
     <div className={styles.container}>
       <p className={styles.signIn}>Ready to join the colony?</p>
-      <Link className={styles.signInBtn} href={"/user/signup"}>
+      <Link
+        onClick={(e) => handleClick(e, "/user/signup")}
+        className={styles.signInBtn}
+        href="/user/signup"
+      >
         Sign Up
       </Link>
       <p className={styles.signIn}>Already a worker bee?</p>
-      <Link className={styles.signInBtn} href={"/user/login"}>
+      <Link
+        onClick={(e) => handleClick(e, "/user/login")}
+        className={styles.signInBtn}
+        href="/user/login"
+      >
         Login
       </Link>
     </div>
   );
 };
-
 export default LoginSignUpCard;
