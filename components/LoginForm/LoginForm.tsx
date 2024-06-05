9e99ffeab920d8f5814559cfff00854dfa959179
Mason Bybee <bybeemason@gmail.com>
2024-06-04 19:53:52 -0500

    updated styling

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -1,15 +1,51 @@
 import { login } from "@/actions/session";
 import React from "react";
+import styles from "./LoginForm.module.css";
+import Link from "next/link";
 
 const LoginForm = () => {
   return (
-    <form className="loginForm" action={login} method="POST">
-      <label htmlFor="username">Username</label>
-      <input placeholder="Username" id="username" type="text" />
-      <label htmlFor="password">Password</label>
-      <input placeholder="Password" id="password" type="password" />
-      <button type="submit">Login</button>
-    </form>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Login</h1>
+      <form className={styles.form} action={login}>
+        <div className={`${styles.inputWrapper} ${styles["md:span-2"]}`}>
+          <input
+            type="username"
+            id="username"
+            className={styles.formInput}
+            placeholder="Username"
+            autoComplete="username"
+          />
+          <label htmlFor="username" className={styles.formLabel}>
+            Username
+          </label>
+        </div>
+        <div className={`${styles.inputWrapper} ${styles["md:span-2"]}`}>
+          <input
+            type="password"
+            id="password"
+            className={styles.formInput}
+            placeholder="Password"
+            autoComplete="password"
+          />
+          <label htmlFor="password" className={styles.formLabel}>
+            Password
+          </label>
+        </div>
+        <button
+          type="submit"
+          className={`${styles.btn} ${styles["md:span-2"]}`}
+        >
+          Sign In
+        </button>
+      </form>
+      <p>
+        New to the hive? Sign up{" "}
+        <Link className={styles.link} href={"/user/signup"}>
+          <b>HERE!</b>
+        </Link>
+      </p>
+    </div>
   );
 };
 
