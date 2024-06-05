d04d78b5d264bc9e6db0f901be3f723a8345ae10
Mason Bybee <bybeemason@gmail.com>
2024-06-04 23:36:39 -0500

    removed api routes

--- app/api/users/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import db from "@/db/db";
-import { users } from "@/schema/User";
-
-export async function GET() {
-  const dbUsers = await db.select().from(users);
-
-  return NextResponse.json({ users: dbUsers });
-}
