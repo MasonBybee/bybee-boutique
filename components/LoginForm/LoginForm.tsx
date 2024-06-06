b0c3387b72a175250c7831e5fc779fd6ba60b879
Mason Bybee <bybeemason@gmail.com>
2024-06-05 19:52:50 -0500

    added react hook form

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -1,24 +1,59 @@
+"use client";
 import { login } from "@/actions/session";
 import React from "react";
 import styles from "./LoginForm.module.css";
 import Link from "next/link";
-
+import { LoginFormValues } from "@/lib/types";
+import { useForm, useFormState } from "react-hook-form";
 const LoginForm = () => {
+  const { control, register, handleSubmit, setError } =
+    useForm<LoginFormValues>();
+  const { errors } = useFormState({ control });
+
+  const onSubmit = async (data: LoginFormValues) => {
+    try {
+      await login(data);
+    } catch (error: Error | any) {
+      if (error.message) console.error(error.message);
+      if (error.response && error.response.data) {
+        const serverErrors = error.response.data.errors;
+        serverErrors.forEach(
+          (serverError: { field: keyof LoginFormValues; message: string }) => {
+            setError(serverError.field, {
+              type: "server",
+              message: serverError.message,
+            });
+          }
+        );
+      } else {
+        // Fallback for unexpected errors
+        setError("username", {
+          type: "server",
+          message: "An unexpected error occurred",
+        });
+      }
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Login</h1>
-      <form className={styles.form} action={login}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={`${styles.inputWrapper} ${styles["md:span-2"]}`}>
           <input
-            type="username"
+            type="text"
             id="username"
             className={styles.formInput}
             placeholder="Username"
             autoComplete="username"
+            {...register("username", { required: "Username is required" })}
           />
           <label htmlFor="username" className={styles.formLabel}>
             Username
           </label>
+          {errors.username && (
+            <p className={styles.error}>{errors.username.message}</p>
+          )}
         </div>
         <div className={`${styles.inputWrapper} ${styles["md:span-2"]}`}>
           <input
@@ -26,11 +61,15 @@ const LoginForm = () => {
             id="password"
             className={styles.formInput}
             placeholder="Password"
-            autoComplete="password"
+            autoComplete="current-password"
+            {...register("password", { required: "Password is required" })}
           />
           <label htmlFor="password" className={styles.formLabel}>
             Password
           </label>
+          {errors.password && (
+            <p className={styles.error}>{errors.password.message}</p>
+          )}
         </div>
         <button
           type="submit"
--- components/SignUpForm/SignUpForm.tsx
+++ components/SignUpForm/SignUpForm.tsx
@@ -1,13 +1,26 @@
-import React from "react";
-import styles from "./SignUpForm.module.css";
+"use client";
 import { signup } from "@/actions/session";
+import styles from "./SignUpForm.module.css";
+import { SignUpFormValues } from "@/lib/types";
 import Link from "next/link";
+import { useForm, useFormState } from "react-hook-form";
 
-const SignUpForm = () => {
+interface SignUpFormProps {
+  signup: (data: SignUpFormValues) => void;
+}
+
+const SignUpForm = ({ handleChange }) => {
+  const { control, register, handleSubmit } = useForm<SignUpFormValues>();
+  const { errors } = useFormState({ control });
+
+  const onSubmit = async (data: SignUpFormValues) => {
+    handleChange(data);
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Signup</h1>
-      <form className={styles.form} action={signup}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.inputWrapper}>
           <input
             type="text"
@@ -15,22 +28,30 @@ const SignUpForm = () => {
             className={styles.formInput}
             placeholder="First Name"
             autoComplete="given-name"
+            {...register("firstName", { required: "First name is required" })}
           />
           <label htmlFor="firstName" className={styles.formLabel}>
             First Name
           </label>
+          {errors.firstName && (
+            <p className={styles.error}>{errors.firstName.message}</p>
+          )}
         </div>
         <div className={styles.inputWrapper}>
           <input
             type="text"
-            id="lastLame"
+            id="lastName"
             className={styles.formInput}
             placeholder="Last Name"
             autoComplete="family-name"
+            {...register("lastName", { required: "Last Name is required" })}
           />
           <label htmlFor="lastName" className={styles.formLabel}>
             Last Name
           </label>
+          {errors.lastName && (
+            <p className={styles.error}>{errors.lastName.message}</p>
+          )}
         </div>
         <div className={`${styles.inputWrapper} ${styles["md:span-2"]}`}>
           <input
@@ -39,22 +60,30 @@ const SignUpForm = () => {
             className={styles.formInput}
             placeholder="Email Address"
             autoComplete="email"
+            {...register("email", { required: "Email Address is required" })}
           />
           <label htmlFor="email" className={styles.formLabel}>
             Email Address
           </label>
+          {errors.email && (
+            <p className={styles.error}>{errors.email.message}</p>
+          )}
         </div>
         <div className={`${styles.inputWrapper} ${styles["md:span-2"]}`}>
           <input
-            type="username"
+            type="text"
             id="username"
             className={styles.formInput}
             placeholder="Username"
             autoComplete="username"
+            {...register("username", { required: "Username is required" })}
           />
           <label htmlFor="username" className={styles.formLabel}>
             Username
           </label>
+          {errors.username && (
+            <p className={styles.error}>{errors.username.message}</p>
+          )}
         </div>
         <div className={`${styles.inputWrapper} ${styles["md:span-2"]}`}>
           <input
@@ -62,22 +91,44 @@ const SignUpForm = () => {
             id="password"
             className={styles.formInput}
             placeholder="Password"
-            autoComplete="password"
+            autoComplete="current-password"
+            {...register("password", { required: "Password is required" })}
           />
           <label htmlFor="password" className={styles.formLabel}>
             Password
           </label>
+          {errors.password && (
+            <p className={styles.error}>{errors.password.message}</p>
+          )}
+        </div>
+        <div className={`${styles.inputWrapper} ${styles["md:span-2"]}`}>
+          <input
+            type="password"
+            id="checkPassword"
+            className={styles.formInput}
+            autoComplete="new-password"
+            placeholder="Check Password"
+            {...register("checkPassword", {
+              required: "Check Password is required",
+            })}
+          />
+          <label htmlFor="checkPassword" className={styles.formLabel}>
+            Check Password
+          </label>
+          {errors.checkPassword && (
+            <p className={styles.error}>{errors.checkPassword.message}</p>
+          )}
         </div>
         <button
           type="submit"
           className={`${styles.btn} ${styles["md:span-2"]}`}
         >
-          Sign In
+          Sign Up
         </button>
       </form>
       <p>
         Returning worker bee? Login{" "}
-        <Link className={styles.link} href={"/user/signup"}>
+        <Link className={styles.link} href="/user/signup">
           <b>HERE!</b>
         </Link>
       </p>
