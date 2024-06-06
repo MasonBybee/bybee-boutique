7c96ab76dfdfcf5b82a3df204379d824b1ecf462
Mason Bybee <bybeemason@gmail.com>
2024-06-05 19:52:12 -0500

    add wishlistId & cartId to session

--- actions/session.ts
+++ actions/session.ts
@@ -1,13 +1,16 @@
 "use server";
 import CustomError from "@/lib/CustomError";
 import db from "@/lib/db/db";
+import { carts } from "@/lib/schema/Cart";
 import { User, users } from "@/lib/schema/User";
+import { wishlists } from "@/lib/schema/Wishlist";
 import {
   sessionOptions,
   SessionData,
   defaultSession,
   ExtendedSession,
 } from "@/lib/session";
+import { LoginFormValues, SignUpFormValues } from "@/lib/types";
 import bcrypt from "bcrypt";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
@@ -35,38 +38,54 @@ export const getSession = async (
       username: session.username,
       isAdmin: session.isAdmin,
       isLoggedIn: session.isLoggedIn,
+      cartId: session.cartId,
+      wishlistId: session.wishlistId,
     };
   }
 };
 
-export const login = async (formData: FormData) => {
+export const login = async (formData: LoginFormValues) => {
+  console.log("form data:", formData);
   try {
     const session = (await getSession()) as ExtendedSession;
 
-    const formUsername = formData.get("username") as string;
-    const formPassword = formData.get("password") as string;
-    const user: User | undefined = await db.query.users.findFirst({
+    const formUsername = formData.username;
+    const formPassword = formData.password;
+    const user = await db.query.users.findFirst({
+      with: { cart: true, wishlist: true },
       where: (users, { eq }) => eq(users.username, formUsername),
     });
 
     if (user && (await bcrypt.compare(formPassword, user.password))) {
+      let cartId = user.cart.id;
+      let wishlistId = user.wishlist.id;
+      if (!cartId) {
+        const id = await db
+          .insert(carts)
+          .values({ userId: user.id })
+          .returning({ cartId: carts.id });
+        cartId = id[0].cartId;
+      }
+      if (!wishlistId) {
+        const id = await db
+          .insert(wishlists)
+          .values({ userId: user.id })
+          .returning({ wishlistId: wishlists.id });
+        wishlistId = id[0].wishlistId;
+      }
       session.userId = user.id;
       session.username = user.username;
       session.firstName = user.firstName;
       session.lastName = user.lastName;
       session.isAdmin = user.admin;
       session.isLoggedIn = true;
+      session.cartId = cartId;
+      session.wishlistId = wishlistId;
       await session.save();
       redirect("/");
-    } else {
-      throw new CustomError(401, "Invalid username or password");
     }
   } catch (e) {
-    if (e instanceof CustomError) {
-      throw e;
-    } else {
-      throw new CustomError(500, "Internal Server Error");
-    }
+    return e;
   }
 };
 
@@ -76,14 +95,18 @@ export const logout = async () => {
   redirect("/");
 };
 
-export async function signup(formData: FormData) {
+export async function signup(formData: SignUpFormValues) {
   try {
     const session = (await getSession()) as ExtendedSession;
-    const formUsername = formData.get("username") as string;
-    const formPassword = formData.get("password") as string;
-    const formFirstName = formData.get("firstName") as string;
-    const formLastName = formData.get("lastName") as string;
-    const formEmail = formData.get("email") as string;
+    const formUsername = formData.username;
+    const formPassword = formData.password;
+    const formCheckPassword = formData.checkPassword;
+    const formFirstName = formData.firstName;
+    const formLastName = formData.lastName;
+    const formEmail = formData.email;
+    if (formCheckPassword !== formPassword) {
+      throw new CustomError(401, "Passwords Must Match");
+    }
     const hashedPassword: string = await bcrypt.hash(formPassword, 12);
     const newUser = {
       username: formUsername,
@@ -97,18 +120,30 @@ export async function signup(formData: FormData) {
       .values(newUser)
       .onConflictDoNothing()
       .returning({ id: users.id });
+    const wishlistId = await db
+      .insert(wishlists)
+      .values({ userId: result[0].id })
+      .returning({ wishlistId: wishlists.id });
+    const cartId = await db
+      .insert(carts)
+      .values({ userId: result[0].id })
+      .returning({ cartId: carts.id });
     if (result) {
       session.userId = result[0].id;
       session.username = formUsername;
       session.firstName = formFirstName;
       session.lastName = formLastName;
       session.isLoggedIn = true;
+      session.wishlistId = Number(wishlistId[0].wishlistId);
+      session.cartId = Number(cartId[0].cartId);
+      session.save();
+      redirect("/");
     }
   } catch (e) {
-    if (e instanceof CustomError) {
-      throw e;
-    } else {
-      throw new CustomError(500, "Internal Server Error");
-    }
+    return e;
   }
 }
+
+export const testFunc = async (data: SignUpFormValues) => {
+  return;
+};
