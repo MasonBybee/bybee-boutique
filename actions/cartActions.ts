2d2c19065005fc43d271e2b940ad93a5d1a258b0
Mason Bybee <bybeemason@gmail.com>
2024-07-12 11:35:35 -0500

    add getCart, getCartProducts, updated addCartItem

--- actions/cartActions.ts
+++ actions/cartActions.ts
@@ -2,8 +2,62 @@
 
 import CustomError from "@/lib/CustomError";
 import db from "@/lib/db/db";
-import { cartItems } from "@/lib/schema/CartItems";
-import { and, eq } from "drizzle-orm";
+import { CartItem, cartItems } from "@/lib/schema/CartItems";
+import { images } from "@/lib/schema/Image";
+import { inventory } from "@/lib/schema/Inventory";
+import { InventoryWithImages } from "@/lib/types";
+import { and, eq, inArray } from "drizzle-orm";
+import { convertToInventoryWithImages } from "./helperActions";
+
+export const getCart = async (cartId: number) => {
+  try {
+    const response = await db.query.cartItems.findMany({
+      where: (cartItems, { eq }) => eq(cartItems.cartId, cartId),
+    });
+    if (response) {
+      return { success: true, data: response };
+    }
+    return { success: false, error: "Error retrieving cart" };
+  } catch (e) {
+    return { success: false, error: "Error retrieving cart" };
+  }
+};
+
+export const getCartProducts = async (cartItemIds: CartItem[]) => {
+  try {
+    const inventoryIds = cartItemIds.map((item) => item.inventoryId);
+    const response = await db
+      .select({
+        inventory: {
+          id: inventory.id,
+          name: inventory.name,
+          description: inventory.description,
+          stock: inventory.stock,
+          unitPrice: inventory.unitPrice,
+          categoryId: inventory.categoryId,
+        },
+        images: {
+          id: images.id,
+          description: images.description,
+          categoryId: images.categoryId,
+          inventoryId: images.inventoryId,
+          image: images.image,
+        },
+      })
+      .from(inventory)
+      .leftJoin(images, eq(images.inventoryId, inventory.id))
+      .where(inArray(inventory.id, inventoryIds));
+    if (response) {
+      const convertedResponse: InventoryWithImages[] =
+        convertToInventoryWithImages(response);
+
+      return { success: true, data: convertedResponse };
+    }
+    return { success: false, error: "Error retrieving cart" };
+  } catch (e) {
+    return { success: false, error: "Error retrieving cart" };
+  }
+};
 
 export const addCartItem = async (
   cartId: number,
@@ -16,12 +70,12 @@ export const addCartItem = async (
       .values({ cartId: cartId, inventoryId: inventoryId, quantity: quantity })
       .onConflictDoNothing()
       .returning();
-    if (!result[0]) {
-      throw new CustomError(409, "Database Conflict");
+    if (result[0]) {
+      return { success: true, data: result };
     }
-    return result[0];
+    return { success: false, error: "Error adding item to cart" };
   } catch (e) {
-    throw new CustomError(500, "Internal Server Error");
+    return { success: false, error: "Error adding item to cart" };
   }
 };
 
