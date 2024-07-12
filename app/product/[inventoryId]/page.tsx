ad6758ef57649a5d740e21aba27a6482f95c17ba
Mason Bybee <bybeemason@gmail.com>
2024-07-12 11:38:10 -0500

    moved form to component

--- app/product/[inventoryId]/page.tsx
+++ app/product/[inventoryId]/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 import WishlistButton from "@/components/WishlistButton";
 import { getSession } from "@/actions/session";
 import { getWishlist } from "@/actions/wishlistActions";
+import ProductForm from "@/components/ProductForm";
 
 const ProductPage = async ({ params }: { params: { inventoryId: number } }) => {
   const product = await getProductWithImage(params.inventoryId);
@@ -27,22 +28,7 @@ const ProductPage = async ({ params }: { params: { inventoryId: number } }) => {
         <h3 className={styles.productName}>{product?.name}</h3>
         <p className={styles.price}>${product?.unitPrice}</p>
         <h5 className={styles.stockLabel}>In Stock: {product?.stock}</h5>
-        <form action="">
-          <label className={styles.sizeLabel} htmlFor="size">
-            Size:
-          </label>
-          <select id="size" name="size">
-            <option value="0">S</option>
-            <option value="1">M</option>
-            <option value="2">L</option>
-            <option value="3">XL</option>
-            <option value="4">XXL</option>
-          </select>
-          <br />
-          <label className={styles.qtyLabel}>QTY:</label>
-          <input className={styles.qtyInput} type="number" />
-          <button className={styles.addToCartButton}>Add To Cart</button>
-        </form>
+        <ProductForm cartId={user.cartId} inventoryId={product?.id!} />
         <h5 className={styles.detailsLabel}>Details:</h5>
         <p>{product?.description}</p>
       </div>
---
+++ components/ProductForm/ProductForm.tsx
@@ -0,0 +1,77 @@
+"use client";
+import React, { ChangeEvent, useState, useTransition } from "react";
+import styles from "./ProductForm.module.css";
+import { addCartItem } from "@/actions/cartActions";
+import { useRouter } from "next/navigation";
+import Loading from "../Loading";
+
+const initialFormState = {
+  size: 1,
+  qty: 1,
+};
+
+interface ProductFormProps {
+  cartId: number;
+  inventoryId: number;
+}
+
+interface ProductDataState {
+  size: number;
+  qty: number;
+}
+
+const ProductForm: React.FC<ProductFormProps> = ({ cartId, inventoryId }) => {
+  const [data, setData] = useState<ProductDataState>(initialFormState);
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = () => {
+    startTransition(() => {
+      addCartItem(cartId, inventoryId, data.qty).then((response) => {
+        if (response.success) {
+          router.push("/");
+        }
+      });
+    });
+  };
+  return (
+    <form action={handleSubmit}>
+      <label className={styles.sizeLabel} htmlFor="size">
+        Size:
+      </label>
+      <select onChange={handleChange} value={data.size} id="size" name="size">
+        <option value="0">S</option>
+        <option value="1">M</option>
+        <option value="2">L</option>
+        <option value="3">XL</option>
+        <option value="4">XXL</option>
+      </select>
+      <br />
+      <label htmlFor="qty" className={styles.qtyLabel}>
+        QTY:
+      </label>
+      <input
+        onChange={handleChange}
+        value={data.qty}
+        className={styles.qtyInput}
+        type="number"
+        name="qty"
+        id="qty"
+      />
+      {isPending ? (
+        <Loading />
+      ) : (
+        <button className={styles.addToCartButton}>Add To Cart</button>
+      )}
+    </form>
+  );
+};
+
+export default ProductForm;
