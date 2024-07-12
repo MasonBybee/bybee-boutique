5c559176f5c45ff512f35c36c24f63994e1a3297
Mason Bybee <bybeemason@gmail.com>
2024-07-11 19:32:56 -0500

    added validation, changed response format

--- actions/session.ts
+++ actions/session.ts
@@ -1,8 +1,7 @@
 "use server";
-import CustomError from "@/lib/CustomError";
 import db from "@/lib/db/db";
 import { carts } from "@/lib/schema/Cart";
-import { User, users } from "@/lib/schema/User";
+import { users } from "@/lib/schema/User";
 import { wishlists } from "@/lib/schema/Wishlist";
 import {
   sessionOptions,
@@ -10,11 +9,23 @@ import {
   defaultSession,
   ExtendedSession,
 } from "@/lib/session";
-import { LoginFormValues, SignUpFormValues } from "@/lib/types";
+import {
+  FormResponse,
+  LoginFormValues,
+  ProfileValues,
+  SignUpFormValues,
+  UpdatePassword,
+} from "@/lib/types";
 import bcrypt from "bcrypt";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import {
+  signUpFormValidator,
+  updatePasswordValidator,
+  updateProfileValidator,
+} from "./formValidation";
+import { eq } from "drizzle-orm";
 
 export const getSession = async (
   plain = false
@@ -36,6 +47,7 @@ export const getSession = async (
       firstName: session.firstName,
       lastName: session.lastName,
       username: session.username,
+      email: session.email,
       isAdmin: session.isAdmin,
       isLoggedIn: session.isLoggedIn,
       cartId: session.cartId,
@@ -44,8 +56,9 @@ export const getSession = async (
   }
 };
 
-export const login = async (formData: LoginFormValues) => {
-  console.log("form data:", formData);
+export const login = async (
+  formData: LoginFormValues
+): Promise<FormResponse> => {
   try {
     const session = (await getSession()) as ExtendedSession;
 
@@ -75,6 +88,7 @@ export const login = async (formData: LoginFormValues) => {
       }
       session.userId = user.id;
       session.username = user.username;
+      session.email = user.email;
       session.firstName = user.firstName;
       session.lastName = user.lastName;
       session.isAdmin = user.admin;
@@ -82,68 +96,133 @@ export const login = async (formData: LoginFormValues) => {
       session.cartId = cartId;
       session.wishlistId = wishlistId;
       await session.save();
-      redirect("/");
+      return { success: true };
+    } else {
+      return {
+        success: false,
+        errors: { submit: "Invalid username or password" },
+      };
     }
   } catch (e) {
-    return e;
+    return {
+      success: false,
+      errors: { submit: "Internal Server Error" },
+    };
   }
 };
 
 export const logout = async () => {
   const session = (await getSession()) as ExtendedSession;
   session.destroy();
-  redirect("/");
 };
 
-export async function signup(formData: SignUpFormValues) {
+export async function signup(
+  formData: SignUpFormValues
+): Promise<FormResponse> {
   try {
-    const session = (await getSession()) as ExtendedSession;
-    const formUsername = formData.username;
-    const formPassword = formData.password;
-    const formCheckPassword = formData.checkPassword;
-    const formFirstName = formData.firstName;
-    const formLastName = formData.lastName;
-    const formEmail = formData.email;
-    if (formCheckPassword !== formPassword) {
-      throw new CustomError(401, "Passwords Must Match");
+    const errors = signUpFormValidator(formData);
+    if (Object.keys(errors).length) {
+      return { success: false, errors: errors };
     }
-    const hashedPassword: string = await bcrypt.hash(formPassword, 12);
+    const session = (await getSession()) as ExtendedSession;
+
+    const hashedPassword: string = await bcrypt.hash(formData.password, 12);
     const newUser = {
-      username: formUsername,
+      username: formData.username,
       password: hashedPassword,
-      firstName: formFirstName,
-      lastName: formLastName,
-      email: formEmail,
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
     };
     const result = await db
       .insert(users)
       .values(newUser)
-      .onConflictDoNothing()
       .returning({ id: users.id });
-    const wishlistId = await db
-      .insert(wishlists)
-      .values({ userId: result[0].id })
-      .returning({ wishlistId: wishlists.id });
-    const cartId = await db
-      .insert(carts)
-      .values({ userId: result[0].id })
-      .returning({ cartId: carts.id });
     if (result) {
+      const wishlistId = await db
+        .insert(wishlists)
+        .values({ userId: result[0].id })
+        .returning({ wishlistId: wishlists.id });
+
+      const cartId = await db
+        .insert(carts)
+        .values({ userId: result[0].id })
+        .returning({ cartId: carts.id });
+
       session.userId = result[0].id;
-      session.username = formUsername;
-      session.firstName = formFirstName;
-      session.lastName = formLastName;
+      session.username = formData.username;
+      session.email = formData.email;
+      session.firstName = formData.firstName;
+      session.lastName = formData.lastName;
       session.isLoggedIn = true;
       session.wishlistId = Number(wishlistId[0].wishlistId);
       session.cartId = Number(cartId[0].cartId);
       session.save();
-      redirect("/");
+      return { success: true };
     }
-  } catch (e) {
-    return e;
+    return {
+      success: false,
+      errors: { submit: "Internal Server Error" },
+    };
+  } catch (e: any) {
+    if (e.code === "23505") {
+      return {
+        success: false,
+        errors: { submit: "Username taken please try again" },
+      };
+    }
+    return {
+      success: false,
+      errors: { submit: e.message },
+    };
   }
 }
 
-export const testFunc = async (data: SignUpFormValues) => {
-  return;
+export const updateProfileSession = async (
+  profileValues: ProfileValues
+): Promise<FormResponse> => {
+  try {
+    const errors = updateProfileValidator(profileValues);
+    if (Object.keys(errors).length) {
+      return { success: false, errors: errors };
+    }
+    const session = await getSession();
+    const updateResult = await db
+      .update(users)
+      .set({
+        firstName: profileValues.firstName,
+        lastName: profileValues.lastName,
+        email: profileValues.email,
+      })
+      .where(eq(users.username, session.username))
+      .returning({ id: users.id });
+    if (!updateResult[0].id) {
+      return { success: false, errors: { submit: "Profile not updated" } };
+    }
+    session.firstName = profileValues.firstName;
+    session.lastName = profileValues.lastName;
+    session.email = profileValues.email;
+    await session.save();
+    return { success: true };
+  } catch (e: any) {
+    console.error("Failed to update session:", e);
+    return {
+      success: false,
+      errors: { submit: e.message || "Failed to update profile" },
+    };
+  }
+};
+
+export const updatePassword = async (formData: UpdatePassword) => {
+  const errors = updatePasswordValidator(formData);
+  if (Object.keys(errors).length) {
+    return { success: false, errors: errors };
+  }
+  const session = await getSession();
+  const hashedPassword: string = await bcrypt.hash(formData.password, 12);
+  await db
+    .update(users)
+    .set({ password: hashedPassword })
+    .where(eq(users.username, session.username));
+  return { success: true };
 };
