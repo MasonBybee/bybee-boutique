a43d300ec25eb7e3c657daa4e16cfc0269287116
Mason Bybee <bybeemason@gmail.com>
2024-07-13 19:09:02 -0500

    add address and payment validation

--- actions/formValidation.ts
+++ actions/formValidation.ts
@@ -1,4 +1,6 @@
 import {
+  AddressAndPaymentFormData,
+  CheckoutFormErrors,
   FormErrors,
   ProfileValues,
   SignUpFormValues,
@@ -64,5 +66,50 @@ export const updatePasswordValidator = (data: UpdatePassword) => {
   if (data.password !== data.checkPassword) {
     errors.checkPassword = "Passwords must match";
   }
+  return errors;
+};
+
+export const addressAndPaymentFormValidator = (
+  data: AddressAndPaymentFormData
+) => {
+  const errors: CheckoutFormErrors = {};
+
+  if (!data.addressLineOne || data.addressLineOne.length < 5) {
+    errors.addressLineOne =
+      "Address Line One must be at least 5 characters long";
+  }
+
+  if (!data.zipCode || !/^\d{5}(-\d{4})?$/.test(data.zipCode)) {
+    errors.zipCode =
+      "Zip Code must be a valid format (e.g., 12345 or 12345-6789)";
+  }
+
+  if (!data.city || data.city.length < 3) {
+    errors.city = "City must be at least 3 characters long";
+  }
+
+  if (!data.state || data.state.length !== 2) {
+    errors.state = "State must be a valid 2-character state code";
+  }
+
+  if (!data.cardNumber || !/^\d{16}$/.test(data.cardNumber)) {
+    errors.cardNumber = "Card Number must be exactly 16 digits";
+  }
+
+  if (!data.fullName || data.fullName.length < 5) {
+    errors.fullName = "Full Name must be at least 5 characters long";
+  }
+
+  if (
+    !data.expirationDate ||
+    !/^(0[1-9]|1[0-2])\/\d{2}$/.test(data.expirationDate)
+  ) {
+    errors.expirationDate = "Expiration Date must be in MM/YY format";
+  }
+
+  if (!data.securityCode || !/^\d{3,4}$/.test(data.securityCode)) {
+    errors.securityCode = "Security Code must be 3 or 4 digits";
+  }
+
   return errors;
 };
