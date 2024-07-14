a26c4af4e4267c900ad8967ac76810f94ed5b5bf
Mason Bybee <bybeemason@gmail.com>
2024-07-13 22:02:30 -0500

    remove unused sections

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,21 +4,37 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.footerContainer}>
-      <div className={styles.footerContent}>
+      {/* <div className={styles.footerContent}>
         <h3>Helpful Links</h3>
         <ul></ul>
-      </div>
-      <div className={styles.footerContent}>
+      </div> */}
+      {/* <div className={styles.footerContent}>
         <h3>About Us</h3>
-        <ul></ul>
+        <ul>
+          <a href="https://www.MasonBybee.com">MasonBybee.com</a>
+        </ul>
+      </div> */}
+      <div className={styles.footerContent}>
+        <h3 style={{ paddingBottom: "20px" }}>My Account</h3>
+        <ul>
+          <li style={{ listStyle: "none", paddingBottom: "20px" }}>
+            <a href="/profile">Profile</a>
+          </li>
+          <li style={{ listStyle: "none", paddingBottom: "20px" }}>
+            <a href="/profile">Cart</a>
+          </li>
+          <li style={{ listStyle: "none", paddingBottom: "20px" }}>
+            <a href="/profile">Wishlist</a>
+          </li>
+        </ul>
       </div>
       <div className={styles.footerContent}>
-        <h3>My Account</h3>
-        <ul></ul>
-      </div>
-      <div className={styles.footerContent}>
-        <h3>Contact Us</h3>
-        <ul></ul>
+        <h3 style={{ paddingBottom: "20px" }}>Contact Us</h3>
+        <ul>
+          <li style={{ listStyle: "none" }}>
+            <a href="https://www.MasonBybee.com">MasonBybee.com</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
